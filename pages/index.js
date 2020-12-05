e43ff01e199396f42088ad0e16056c7c88693056
Klesio <klesio.k9@gmail.com>
2020-12-04 21:50:19 -0300

    fix erro v0.5

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 function HomePage() {
   return (
-  <div>
-    <h1>Home</h1>
-    <a href="/sobre.js">Acessar Página Sobre</a>
-  </div>
-  )
+    <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Home</title>
+      </head>
+      <body>
+        <h1>Home</h1>
+        <a href="/sobre.js">Acessar página Sobre</a>
+      </body>
+    </html>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,10 +1,10 @@
 function Sobre() {
   return (
-  <div>
-    <h1>Sobre</h1>
-    <a href="/index.js">Acessar Página Home</a>
-  </div>
-  )
+    <div>
+      <h1>Sobre</h1>
+      <a href="/index.js">Acessar página Home</a>
+    </div>
+  );
 }
 
-export default Sobre
+export default Sobre;
